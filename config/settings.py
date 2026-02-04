818979fc87570c24e7f57f7a721d609b6d07a940
Ayushweb16 <dongareayush16@gmail.com>
2026-02-04 21:21:10 +0530

    Fix settings.py: use decouple and allow all hosts for Vercel

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nq%n-=v9iw4q9ejwst#59kkk$rud(8k+&-blqtf2un_v70+rwq'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-nq%n-=v9iw4q9ejwst#59kkk$rud(8k+&-blqtf2un_v70+rwq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 
 # Application definition
